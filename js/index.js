3a619f72713b2d8a6552e8617e13504c794bffd3
Joseph Pamintuan <you@example.com>
2017-11-03 13:09:15 -0700

    1st

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+    // Device Event Listener
+   
+    
+        document.addEventListener('deviceready', onDeviceReady, false);
+    
+    
+   function onDeviceReady () {
+        console.log('Device is Ready');
+    }
+ 
